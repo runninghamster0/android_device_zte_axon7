a67689fb124f38d623d57ad942bedbe443d1e4f9
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    axon7: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -381,6 +381,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
