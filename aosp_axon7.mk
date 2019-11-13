ff93182b40cd96d098668bfeae32db7b8038196c
runninghamster0 <grojbandpp@gmail.com>
2019-11-13 07:56:24 +0000

    axon7: PixelExperience bring-up.

--- aosp_axon7.mk
+++ aosp_axon7.mk
@@ -22,22 +22,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ailsa_ii device
 $(call inherit-product, device/zte/axon7/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_axon7
+PRODUCT_NAME := aosp_axon7
 PRODUCT_DEVICE := axon7
 PRODUCT_BRAND := ZTE
-PRODUCT_MODEL := ZTE A2017U
+PRODUCT_MODEL := ZTE A2017X
 PRODUCT_MANUFACTURER := ZTE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="ailsa_ii" \
-    PRODUCT_NAME="P996A01_O" \
-    PRIVATE_BUILD_DESC="P996A01_O-user 8.0.0 OPR1.170623.032 28 release-keys"
+    PRODUCT_NAME="axon7" \
+    PRIVATE_BUILD_DESC="OnePlus-OnePlus3 9 PKQ1.181203.001 1907311932 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "ZTE/P996A01_O/ailsa_ii:8.0.0/OPR1.170623.032/20180815.171922:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-zte
