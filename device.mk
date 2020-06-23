afb2a56ab72b2b7ba3661c290d0d7f8a03277400
lightvortex <rishishukla242@gmail.com>
2020-06-23 12:34:41 +0530

    axon7:add custom camera provider hal

--- device.mk
+++ device.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service.axon7 \
     camera.device@3.2-impl \
     libshim_camera \
     Snap \
