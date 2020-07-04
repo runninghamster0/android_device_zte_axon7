9859e15d1b307639f10ec044a4f6785049c7db7e
runninghamster0 <grojbandpp@gmail.com>
2020-07-04 02:55:30 +0000

    Revert "axon7:import oss camera hal"
    This reverts commit 12eb64a3ac231242549786a5a8880fc77d65b755.

--- device.mk
+++ device.mk
@@ -163,12 +163,14 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    android.frameworks.displayservice@1.0 \
-    camera.msm8996 \
+    android.hardware.camera.provider@2.4-service.axon7 \
+    camera.device@3.2-impl \
+    libshim_camera \
     Snap \
-    libmm-qcamera
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
