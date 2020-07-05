2cb6a16e868ca4e6c476b3d3ed56e487bbfb6fc0
runninghamster0 <grojbandpp@gmail.com>
2020-07-05 03:03:34 +0000

    axon7: Remove snap for PixelExperience

--- device.mk
+++ device.mk
@@ -168,7 +168,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     libshim_camera \
-    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
