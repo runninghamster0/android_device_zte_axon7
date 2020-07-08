d31d12f5a2b87710a15b9227bd6942277dde1ac6
runninghamster0 <grojbandpp@gmail.com>
2020-07-08 06:45:08 +0000

    axon7: Fixed correct system partition name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ BUILD_BROKEN_DUP_RULES := true
 # System-As-Root
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_KERNEL_CMDLINE += skip_initramfs rootwait ro init=/init root=/dev/dm-0
-BOARD_KERNEL_CMDLINE += dm=\"system none ro,0 1 android-verity /dev/mmcblk0p1\"
+BOARD_KERNEL_CMDLINE += dm=\"system none ro,0 1 android-verity /dev/sde13\"
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
