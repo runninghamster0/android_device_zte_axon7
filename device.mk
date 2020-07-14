6880e18f9d7b66e90f2ebd5395740f8f947b4a9d
runninghamster0 <grojbandpp@gmail.com>
2020-07-14 12:10:42 +0000

    axon7: Build telephony-ext

--- device.mk
+++ device.mk
@@ -391,6 +391,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Telephony
+PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
