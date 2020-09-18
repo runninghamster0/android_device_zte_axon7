135d2e84e1c369f6f8ce76b989a103f0d3cb62cd
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    axon7: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,7 +218,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 VENDOR_SECURITY_PATCH := 2018-07-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
